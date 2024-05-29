256a591468d9a75c6cf8c3d684a7372a097d982a
Jithin Vishnu R <jvjithin2@gmail.com>
2024-05-29 23:19:34 +0530

    enhanced fe security and accessibility

--- src/App.js
+++ src/App.js
@@ -1,56 +1,130 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css';
-import profileImg from './assets/Jithin.jpg';
+import profileImg from './assets/images/Jithin.jpg';
 
 const App = () => {
-  // Retrieve the dark mode state from localStorage
   const [isDarkMode, setIsDarkMode] = useState(() => {
     const savedMode = localStorage.getItem('darkMode');
     return savedMode ? JSON.parse(savedMode) : true;
   });
 
   const [isLoaded, setIsLoaded] = useState(false);
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  const footerRef = useRef(null);
+  const [footerOffset, setFooterOffset] = useState(0);
+
+  const checkScrollTop = useCallback(() => {
+    if (!showScrollButton && window.pageYOffset > window.innerHeight) {
+      setShowScrollButton(true);
+    } else if (showScrollButton && window.pageYOffset <= window.innerHeight) {
+      setShowScrollButton(false);
+    }
+  }, [showScrollButton]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTop);
+    return () => window.removeEventListener('scroll', checkScrollTop);
+  }, [checkScrollTop]);
+
+  const checkScrollTopAndFooter = useCallback(() => {
+    const footerHeight = footerRef.current?.offsetHeight || 0;
+    const scrollPosition = window.pageYOffset;
+    const windowHeight = window.innerHeight;
+    const bodyHeight = document.body.offsetHeight;
+
+    if (bodyHeight - footerHeight - scrollPosition < windowHeight) {
+      setFooterOffset(footerHeight);
+    } else {
+      setFooterOffset(0);
+    }
+
+    setShowScrollButton(scrollPosition > windowHeight);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScrollTopAndFooter);
+    return () => window.removeEventListener('scroll', checkScrollTopAndFooter);
+  }, [checkScrollTopAndFooter]);
 
   const toggleDarkMode = () => {
     setIsDarkMode(prevMode => {
       const newMode = !prevMode;
-      localStorage.setItem('darkMode', JSON.stringify(newMode)); // Save the new mode to localStorage
+      localStorage.setItem('darkMode', JSON.stringify(newMode));
       return newMode;
     });
   };
 
   useEffect(() => {
-    setIsLoaded(true); // Set loaded state to true to trigger animation
+    setIsLoaded(true);
   }, []);
 
   return (
     <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+      <Helmet>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; img-src 'self' data:; connect-src 'self'; font-src 'self' https://cdnjs.cloudflare.com; object-src 'none'; upgrade-insecure-requests;" />
+        <meta http-equiv="X-Content-Type-Options" content="nosniff" />
+        <meta http-equiv="Referrer-Policy" content="no-referrer-when-downgrade" />
+        <meta http-equiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains; preload" />
+        <meta http-equiv="X-Frame-Options" content="DENY" />
+        <meta http-equiv="X-XSS-Protection" content="1; mode=block" />
+      </Helmet>
       <div className="container">
         <header>
           <div className="profile-header">
-            <img src={profileImg} alt="Profile" className="profile-picture-small" />
+            <img src={profileImg} alt="Selfie of Jithin" className="profile-picture-small" />
             <h1>Jithin</h1>
           </div>
-          <div className="toggle-switch" onClick={toggleDarkMode}>
+          <div
+            className="toggle-switch"
+            role="button"
+            tabIndex={0}
+            title='Theme toggle'
+            onClick={toggleDarkMode}
+            onKeyPress={(e) => e.key === 'Enter' && toggleDarkMode()}
+            aria-pressed={isDarkMode}
+            aria-label={isDarkMode ? "Toggle light mode" : "Toggle dark mode"}
+          >
             <div className={`toggle-knob ${isDarkMode ? 'dark' : 'light'}`}></div>
           </div>
         </header>
         <main className={isLoaded ? 'loaded' : ''}>
-          <img src={profileImg} alt="Profile" className="profile-picture-large" />
-          <h2>Jithin Vishnu R</h2>
-          <p>I am a Software Development Engineer in Test with over 4+ years of experience in the tech industry specializing in QA & Automation.</p>
-        </main>
-        <footer>
-          <p>Jithin</p>
-          <div className="social-icons">
-            <a href="https://linkedin.com/in/jvjithin" rel='noreferrer' target='_blank'><i className="fa-brands fa-linkedin-in"></i></a>
-            <a href="https://github.com/jvjithin" rel='noreferrer' target='_blank'><i className="fa-brands fa-github"></i></a>
-            <a href="https://facebook.com/jitviz" rel='noreferrer' target='_blank'><i className="fa-brands fa-facebook-f"></i></a>
-            <a href="https://instagram.com/jit_viz" rel='noreferrer' target='_blank'><i className="fa-brands fa-instagram"></i></a>
-            <a href="https://x.com/jitviz" rel='noreferrer' target='_blank'><i className="fa-brands fa-x-twitter"></i></a>
+          <div className='main-content'>
+            <img src={profileImg} alt="Selfie of Jithin" className="profile-picture-large" />
+            <h2>Jithin Vishnu R</h2>
+            <p>I am a Software Development Engineer in Test with over 4+ years of experience in the tech industry specializing in QA & Automation.</p>
+            {Array(50).fill().map((_, index) => <p key={index}></p>)}
           </div>
-        </footer>
+          {showScrollButton && (
+            <button
+              className="scroll-to-top"
+              title='Scroll to top'
+              onClick={scrollToTop}
+              style={{ position: 'fixed', bottom: `${20 + footerOffset}px`, right: '20px' }}
+              aria-label="Scroll to top"
+            >
+              <i className="fa-solid fa-angles-up"></i>
+            </button>
+          )}
+        </main>
       </div>
+      <footer ref={footerRef}>
+        <p>Jithin</p>
+        <div className="social-icons">
+          <a href="https://linkedin.com/in/jvjithin" rel="noopener noreferrer" target="_blank" aria-label="LinkedIn"><i className="fa-brands fa-linkedin-in"></i></a>
+          <a href="https://github.com/jvjithin" rel="noopener noreferrer" target="_blank" aria-label="GitHub"><i className="fa-brands fa-github"></i></a>
+          <a href="https://facebook.com/jitviz" rel="noopener noreferrer" target="_blank" aria-label="Facebook"><i className="fa-brands fa-facebook-f"></i></a>
+          <a href="https://instagram.com/jit_viz" rel="noopener noreferrer" target="_blank" aria-label="Instagram"><i className="fa-brands fa-instagram"></i></a>
+          <a href="https://x.com/jitviz" rel="noopener noreferrer" target="_blank" aria-label="Twitter"><i className="fa-brands fa-x-twitter"></i></a>
+        </div>
+      </footer>
     </div>
   );
 };
